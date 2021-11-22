6bb2338f308869464b48d0447f9213195c2e3cb4
Ick Hyun Kwon <kwonih1020@gmail.com>
2021-11-22 13:13:35 -0500

    added react router

--- shopping-react/src/App.js
+++ shopping-react/src/App.js
@@ -1,8 +1,11 @@
+/* eslint-disable */
 import React, { useState } from "react";
 import { Button, Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Data from "./data.js";
+
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [shoes, shoesChange] = useState(Data);
@@ -11,7 +14,7 @@ function App() {
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Shopping Mall</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -34,28 +37,52 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="jumbotron m-3 background">
-        <h1 className="display-4">20% Season Off</h1>
-        <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
 
-        <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">
-          Learn more
-        </a>
-      </div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Shoescard shoes={shoes[i]} i={i}></Shoescard>;
-          })}
+      <Route exact path="/">
+        <div>Main Page</div>
+        <div className="jumbotron m-3 background">
+          <h1 className="display-4">20% Season Off</h1>
+          <p className="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+          <a className="btn btn-primary btn-lg" href="#" role="button">
+            Learn more
+          </a>
         </div>
-      </div>
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return <Shoescard shoes={shoes[i]} i={i}></Shoescard>;
+            })}
+          </div>
+        </div>
+      </Route>
+      <Route path="/detail">
+        <div>Detail Page</div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">Detail</h4>
+              <p>Product Detail</p>
+              <p>$120000</p>
+              <button className="btn btn-danger">Order</button>
+            </div>
+          </div>
+        </div>
+      </Route>
+      {/* <Route path='' component={Modal}></Route> */}
     </div>
   );
 }
